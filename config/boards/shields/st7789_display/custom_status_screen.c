af2ef0f54a79af700bf7c17e81ee59cb636ffbbe
siegfriedwhisper <46621235+siegfriedwhisper@users.noreply.github.com>
2026-06-06 22:47:05 +0800

    chore: comment out layer display

--- config/boards/shields/st7789_display/custom_status_screen.c
+++ config/boards/shields/st7789_display/custom_status_screen.c
@@ -76,8 +76,8 @@ lv_obj_t *zmk_display_status_screen() {
     zmk_widget_splash_init();
     zmk_widget_output_status_init();
     zmk_widget_peripheral_battery_status_init();
-    zmk_widget_layer_init();
-    start_layer_status();
+        // zmk_widget_layer_init();
+    // start_layer_status();
     zmk_widget_action_button_init();
         // zmk_widget_wpm_init();
         // zmk_widget_modifier_init();
