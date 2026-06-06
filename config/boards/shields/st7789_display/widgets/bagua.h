7b8d15aa468518ccf242f963e5e97229a9734432
siegfriedwhisper <46621235+siegfriedwhisper@users.noreply.github.com>
2026-06-06 20:19:31 +0800

    fix: update bagua.h with missing function declarations

--- config/boards/shields/st7789_display/widgets/bagua.h
+++ config/boards/shields/st7789_display/widgets/bagua.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include <stdint.h>
+#include <stdbool.h>
 
 void zmk_widget_bagua_init(void);
 void start_bagua(void);
 void stop_bagua(void);
 void bagua_set_active_profile(uint8_t profile);
+void bagua_set_usb_connected(bool connected);
+void bagua_set_num_lock(bool on);
+void bagua_set_profile_state(bool connected, bool bonded);
